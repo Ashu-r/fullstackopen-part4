c3d872020017a27ae6628bdc21d4fd151532dfb5
Ashu-r <randiveashutosh@gmail.com>
2020-10-12 18:11:03 +0530

    4.13, 4.14: blog list expansions

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -17,4 +17,21 @@ blogRouter.post("/", async (request, response) => {
 	response.status(201).json(result);
 });
 
+blogRouter.delete("/:id", async (request, response) => {
+	await Blog.findByIdAndDelete(request.params.id);
+	response.status(204).end();
+});
+
+blogRouter.put("/:id", async (request, response) => {
+	const blog = {
+		likes: request.body.likes,
+	};
+	const updatedBlog = await Blog.findByIdAndUpdate(
+		request.params.id,
+		blog,
+		{ new: true }
+	);
+	response.json(updatedBlog.toJSON());
+});
+
 module.exports = blogRouter;
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const http = require('http');
+const http = require("http");
 
-const app = require('./app');
+const app = require("./app");
 
-const logger = require('./utils/logger');
-const config = require('./utils/config');
+const logger = require("./utils/logger");
+const config = require("./utils/config");
 
 const server = http.createServer(app);
 
-const PORT = 3003;
-server.listen(PORT, () => {
+server.listen(config.PORT, () => {
 	logger.info(`Server running on port ${config.PORT}`);
 });
