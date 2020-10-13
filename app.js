392031855af0172dc0fa0023f655e8e5e8c14037
Ashu-r <randiveashutosh@gmail.com>
2020-10-13 23:00:41 +0530

    bloglist expansion, step 6

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const express = require("express");
-require("express-async-errors");
+const express = require('express');
+require('express-async-errors');
 
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
-const mongoose = require("mongoose");
-const logger = require("./utils/logger");
-const config = require("./utils/config");
-const blogRouter = require("./controllers/blogs");
-const usersRouter = require("./controllers/users");
-const middleware = require("./utils/middleware");
+const mongoose = require('mongoose');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
+const blogRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+const middleware = require('./utils/middleware');
 
 const mongoUrl = config.MONGODB_URI;
 mongoose
@@ -20,19 +21,21 @@ mongoose
 		useCreateIndex: true,
 	})
 	.then(() => {
-		logger.info("connected to MongoDB");
+		logger.info('connected to MongoDB');
 	})
 	.catch((error) => {
-		logger.error("error connecting to MongoDB", error.message);
+		logger.error('error connecting to MongoDB', error.message);
 	});
 
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.use("/api/blogs", blogRouter);
+app.use('/api/login', loginRouter);
 
-app.use("/api/users", usersRouter);
+app.use('/api/blogs', blogRouter);
+
+app.use('/api/users', usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
